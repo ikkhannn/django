a448aaf5372c52a03b8bd116281d053b53d2a54f
imran <imrankhannn81@gmail.com>
2020-04-28 23:37:57 +0500

    django rest api

---
+++ djangoapi/api_basic/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Article
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=Article
+#        fields=['id','title','author']
+        fields='__all__'
+
+
+
+#code is commented below because it was built for normal serialization instead of modelserializer
+    # title=serializers.CharField(max_length=100)
+    # author=serializers.CharField(max_length=100)
+    # email=serializers.EmailField(max_length=100)
+    # date=serializers.DateTimeField()
+
+
+    # def create(self,validated_data):
+    #     return Article.objects.create(validated_data)
+
+    # def update(self,instance,validated_data):
+    #     instance.title=validated_data.get('title',instance.title)
+    #     instance.author=validated_data.get('author',instance.author)
+    #     instance.email=validated_data.get('email',instance.email)
+    #     instance.date=validated_data.get('date',instance.date)
+    #     instance.save()
+    #     return instance
